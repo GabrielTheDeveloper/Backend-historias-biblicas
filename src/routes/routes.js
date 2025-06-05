43ec17a5dc0c1f849849b5d1413b140bb5c394bb
GabrielD3vSilva <gabrielld3vv@gmail.com>
2025-06-04 22:49:22 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,6 +12,6 @@ routes.put('/update-history/:id', HistoryController.updateHistory);
 routes.get('/history/:id', HistoryController.getHistoryById);
 routes.get('/histories', HistoryController.getAllHistories);
 routes.post('/create-payment', PaymentAndroidController.CreateAPayment);
-routes.post('/receive-notification', PaymentAndroidController.receiveNotification);
+routes.post('/receive-notification', PaymentAndroidController.receivePaymentStatus);
 
 module.exports = routes;
