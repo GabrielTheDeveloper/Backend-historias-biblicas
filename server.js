7ab6e09bfb08fa4267b78c5351637074d04c0769
GabrielD3vSilva <gabrielld3vv@gmail.com>
2025-06-05 09:26:07 -0300

    Correção na maneira de invocar o websocket

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
-
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectToDB = require('./src/config/Db');
 const routes = require('./src/routes/routes');
+const PaymentAndroidController = require('./src/controller/PaymentsAndroidController');
 
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+PaymentAndroidController.configureWebSocket(server);
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
-
 connectToDB();
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 const UserController = require('../controller/UserController');
 const HistoryController = require('../controller/HistoryController');
 const PaymentAndroidController = require('../controller/PaymentsAndroidController');
-
 const routes = require('express').Router();
 
 routes.post('/login', UserController.login);
 routes.post('/register', UserController.createUser);
+
 routes.post('/create-history', HistoryController.createHistory);
 routes.delete('/delete-history/:id', HistoryController.deleteHistory);
 routes.put('/update-history/:id', HistoryController.updateHistory);
@@ -15,6 +15,5 @@ routes.get('/histories', HistoryController.getAllHistories);
 routes.post('/create-payment', PaymentAndroidController.CreateAPayment);
 routes.post('/payment/confirm', PaymentAndroidController.confirmPayment);
 routes.post('/payment/status', PaymentAndroidController.receivePaymentStatus);
-routes.post('/payment/webhook', PaymentAndroidController.handleStripeWebhook);
 
 module.exports = routes;
